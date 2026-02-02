787a7c28409121599fbc51206820f02a7220e83d
Lil_Carpi <carpi@mailcarpi.xyz>
2026-02-02 19:12:03 +0100

    Añadido comentarios para el generador.php

--- ACT02/generador.php
+++ ACT02/generador.php
@@ -1,27 +1,44 @@
 <?php
 
-declare(strict_types=1);
-$metodo = $_SERVER['REQUEST_METHOD'];
+declare(strict_types=1); // Declara variables estrictas
+$metodo = $_SERVER['REQUEST_METHOD']; // Pide el metodo de envio del formulario
 
-$nombre = $metodo === 'POST'
-  ? ($_POST['nombre'] ?? 'Ánonimo')
-  : ($_GET['nombre'] ?? 'Ánonimo');
 
-$secciones = $metodo === 'POST'
-  ? intval($_POST['secciones'] ?? 5)
+// Verificador del nombre que se envia
+// 
+$nombre = $metodo === 'POST' 
+  ? ($_POST['nombre'] ?? 'Ánonimo') // Si no se proporciona uno, es Ánonimo por defecto \Para POST
+  : ($_GET['nombre'] ?? 'Ánonimo'); // Para GET
+// Concepto
+// Lo que se ha pasado en nombre es exactamente con el metodo 'POST'?
+// Si es que la respuesta es SI, pilla el valor que se encuentre en nombre pasado con POST. Si no existe, usa "Ánonimo".
+// Si es que la respuesta es NO, pilla el valor que se encuentre en nombre pasado con GET. Si no existe, usa "Ánonimo".
+
+
+// Verificador de la cantidad de secciones que se envia
+//
+$secciones = $metodo === 'POST' 
+  ? intval($_POST['secciones'] ?? 5) // Si no se proporciona uno, usa por defecto 5. 
   : intval($_GET['secciones'] ?? 5);
+// Concepto
+// Lo que se ha pasado en secciones es exactamente con el metodo 'POST'?
+// Si es que la respuesta es SI, convierte el valor en entero y pilla lo que se encuentre en secciones pasado con POST. Si no existe, usa 5.
+// Si es que la respuesta es NO, convierte el valor en entero y pilla lo que se encuentre en secciones pasado con GET. Si no existe, usa 5.
 
-$secciones = max(2, min(10, $secciones));
+$secciones = max(2, min(10, $secciones)); // Seguridad: Obliga a que los saltos sean solo de entre 2 a 10 para que la maquina
+                                          // no se haga un Harakiri.
 
-function miDec2Hex(int $numero): string
+function miDec2Hex(int $numero): string // Funcion de conversion a base 16 para el color hexadecimal
+// Entra como integer por la variable numero y sale como string
+// En el extraño caso que el valor que entre no sea un integer o no salga como un string, dara error.
 {
-  $hexDigits = "0123456789abcdef";
-  if ($numero === 0) return "00";
+  $hexDigits = "0123456789abcdef"; // Los digitos disponibles del hexadecimal
+  if ($numero === 0) return "00"; // En caso de que el numero sea 0, devuelve "00". Hexa necesita de dos digitos para operar
 
-  $hex = "";
-  while ($numero > 0) {
-    $resto = $numero % 16;
-    $hex = $hexDigits[$resto] . $hex;
+  $hex = ""; // inicia el string hex, para ir poniendo los valores
+  while ($numero > 0) { 
+    $resto = $numero % 16; // Se usa el resto para seleccionar el digito hexadecimal
+    $hex = $hexDigits[$resto] . $hex; 
     $numero = intdiv($numero, 16);
   }
 
